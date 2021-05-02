a87eebcccc2f1fdaa9c18fe4203486dad452b69a
haneen <haneen.khasawneh96@gmail.com>
2021-05-02 18:56:18 +0300

    lab 26

---
+++ react-app/src/footer.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+class Footer extends React.Component {
+
+    render()
+    {
+    return (
+        <footer>
+            &copy; 2018 Code Fellows
+        </footer>
+    );
+  };
+
+}
+
+export default Footer;
